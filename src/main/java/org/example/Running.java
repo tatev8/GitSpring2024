586c77378d7d24d64edbe3d31a6e8ec56769c6c1
tatev <tatev4592@gmail>
2024-08-10 23:02:04 +0300

    gonna sleep!

--- src/main/java/org/example/Running.java
+++ src/main/java/org/example/Running.java
@@ -4,5 +4,6 @@ public class Running {
     public static void main(String[] args) {
         System.out.println("hello Tatev!");
         System.out.println("hi");
+        System.out.println("Do you want to sleep, Alihan?");
     }
 }
