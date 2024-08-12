72f0a7f6ae0e44511a7d9e501c7abdbf790f4b1c
tatev <tatev4592@gmail>
2024-08-12 23:20:29 +0300

    string2/count hi

---
+++ src/main/java/codingbat/string2/DoubleChar.java
@@ -0,0 +1,10 @@
+package codingbat.string2;
+
+public class DoubleChar {
+    public String doubleChar(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i=0; i<str.length(); i++){
+            result.append(str.charAt(i)).append(str.charAt(i));
+        }return result.toString();
+    }
+}
